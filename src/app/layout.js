8e85a14896638bc5d0d95cb40ab3f66929decebb
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-08-20 22:46:14 +0700

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 export const metadata = {
   title: 'Route Boss',
   description: 'Helpful tool for managing your routes',
+  icons: '/favicon.png',
 }
 
 export default function RootLayout({ children }) {
